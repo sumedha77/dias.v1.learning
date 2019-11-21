a70a9f2fe1754af65f7bc050674f9fd9dea60b43
Chirag Gupta <chirag.gupta@diaspark.com>
2019-11-21 12:56:03 +0530

    first commit

---
+++ src/test/java/TestngLearning.java
@@ -0,0 +1,15 @@
+import org.testng.annotations.*;
+
+public class TestngLearning {
+
+    @BeforeSuite
+    public void beforeSuite(){
+        System.out.println("This is Before Suite");
+    }
+
+    @Test
+    public void test(){
+        System.out.println(" This is test");
+    }
+
+}
