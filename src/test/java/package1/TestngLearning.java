40996c5724cd66b334b45837e46ffae60a97fc75
chirag gupta77 <chiraggupta77@gmail.com>
2019-11-21 17:53:55 +0530

    +Chirag | removed unwanted file

--- src/test/java/package1/TestngLearning.java
+++ src/test/java/package1/TestngLearning.java
@@ -4,14 +4,65 @@ import org.testng.annotations.*;
 
 public class TestngLearning {
 
-    @BeforeSuite
-    public void beforeSuite(){
-        System.out.println("This is Before Suite");
+    @Test
+    public void testCase1() {
+
+        System.out.println("testCase1");
     }
 
     @Test
-    public void test(){
-        System.out.println(" This is test");
+    public void testCase2() {
+
+        System.out.println("testCase2");
     }
+
+    @BeforeMethod
+    public void beforeMethod() {
+
+        System.out.println("beforeMethod");
+    }
+
+    @AfterMethod
+    public void afterMethod() {
+
+        System.out.println("afterMethod");
+    }
+
+    @BeforeClass
+    public void beforeClass() {
+
+        System.out.println("beforeClass");
+    }
+
+    @AfterClass
+    public void afterClass() {
+
+        System.out.println("afterClass");
+    }
+
+    @BeforeTest
+    public void beforeTest() {
+
+        System.out.println("beforeTest");
+    }
+
+    @AfterTest
+    public void afterTest() {
+
+        System.out.println("afterTest");
+    }
+
+    @BeforeSuite
+    public void beforeSuite() {
+
+        System.out.println("beforeSuite");
+    }
+
+    @AfterSuite
+    public void afterSuite() {
+
+        System.out.println("afterSuite");
+    }
+
 
 }
