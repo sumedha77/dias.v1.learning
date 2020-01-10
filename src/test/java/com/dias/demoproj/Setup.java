146790eedf97acc22eec771bfec73b3f8f399bd5
chiraggupta <chiraggupta77@gmail.com>
2020-01-10 15:12:14 +0530

    +Chirag | fixed the code

--- src/test/java/com/dias/demoproj/Setup.java
+++ src/test/java/com/dias/demoproj/Setup.java
@@ -11,31 +11,32 @@ public class Setup {
 	 * Class to provide driver
 	 *
 	 */
-	public static WebDriver driver = null;
+	public  WebDriver driver = null;
 	public static final String CHROME_WEBDRIVE_PROPERTY_KEY="webdriver.chrome.driver";
 //	public static final String FIREFOX_WEBDRIVE_PROPERTY_KEY="webdriver.firefox.bin";
 //	public static final String IE_WEBDRIVE_PROPERTY_KEY="webdriver.ie.driver";
 
-	public static final String CHROME_WEBDRIVE_PROPERTY_VALUE="..\\resources\\test\\chromedriver.exe";
+	public static final String CHROME_WEBDRIVE_PROPERTY_VALUE="D:\\projects\\Diasparklearning\\src\\resources\\test\\chromedriver.exe";
 //	public static final String FIREFOX_WEBDRIVE_PROPERTY_VALUE="C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe";
 //	public static final String IE_WEBDRIVE_PROPERTY_VALUE="\\chromedriver_win32\\IEDriverServer.exe";
 //	
 	
 	@BeforeSuite
-	public static void getDriver() throws Exception {
-	WebDriver driver=new ChromeDriver();
-	System.setProperty(CHROME_WEBDRIVE_PROPERTY_KEY,
-			CHROME_WEBDRIVE_PROPERTY_VALUE);
-	System.out
-			.println(System.getProperty(CHROME_WEBDRIVE_PROPERTY_KEY));
-	driver.get("www.google.com");
+	public  void getDriver() throws Exception {
+		System.setProperty(CHROME_WEBDRIVE_PROPERTY_KEY,
+				CHROME_WEBDRIVE_PROPERTY_VALUE);
+		System.out
+				.println(System.getProperty(CHROME_WEBDRIVE_PROPERTY_KEY));
+	 driver = new ChromeDriver();
+	// driver.get("www.google.com");
+	driver.navigate().to("https://www.google.com/");
 	}
 
 	/**
 	*After Suite
 	*/
 	@AfterSuite
-	public static void afterClass() {
+	public  void afterClass() {
 	driver.quit();
 	}
 }
